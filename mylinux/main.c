d65da05dd91911e7ff48e0b04b9a32232015e968
trueman <zzrtrue@163.com>
2017-11-26 16:10:56 +0800

    initial commit

---
+++ mylinux/main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+#include"smsh.h"
+#define DEL_PROMPT ">"
+int main()
+{
+	char *cmdline, *prompt, **arglist;
+	int result;
+	void setup();
+	prompt = DEL_PROMPT;
+	setup();
+
+	while ((cmdline = next_cmd(prompt, stdin)) != NULL) {
+		if ((arglist = splitline(cmdline)) != NULL) {
+			result = process(arglist);
+			freelist(arglist);
+		}
+		free(cmdline);
+	}
+	return 0;
+}
+void setup() {
+	extern char ** environ;
+	VLenviron2table(environ);
+	signal(SIGINT, SIG_IGN);
+	signal(SIGQUIT, SIG_IGN);
+}
+void fatal(char * s1, char * s2, int n) {
+	fprintf(stderr, "error:%s,%s\n", s1, s2);
+	exit(n);
+}
